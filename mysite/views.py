a6baf501cc656ad1e3f2604608d35d360c7d853a
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-12-09 15:35:08 +0600

    implement django template-cot16

--- mysite/views.py
+++ mysite/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse('<h3>This is our homepage</h3>');
-
+       #return HttpResponse('<h3>This is our homepage</h3>');
+    return render(request, "home.html")
